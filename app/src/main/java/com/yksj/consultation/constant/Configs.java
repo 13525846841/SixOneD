90dbf06121f60ce4e27d2851b0afb71e29b04643
Laddie <762900942@qq.com>
2018-12-06 14:03:59 +0800

    删除不必要的地址

--- app/src/main/java/com/yksj/consultation/constant/Configs.java
+++ app/src/main/java/com/yksj/consultation/constant/Configs.java
@@ -7,28 +7,14 @@ import com.yksj.consultation.sonDoc.BuildConfig;
  * Used for socket,http地址端口链接
  */
 public class Configs {
-    //六一正式
+    //本地测试
 //    public static int SOCKET_PORT = 8014;//Socket端口
 //    public static String SOCKET_IP = "www.61120.cn";
 //    public static String WEB_IP = "https://www.61120.cn";
 
-//    public static int SOCKET_PORT = 8014;//Socket端口
-//    public static String SOCKET_IP = "www.61120.vip";
-//    public static String WEB_IP = "https://www.61120.vip";
-
-//    六一测试
-//    public static int SOCKET_PORT = 8015;//Socket端口
-//    public static String SOCKET_IP = "61.jiansion.com";
-//    public static String WEB_IP = "https://61.jiansion.com";
-
-    //本地测试
-    public static int SOCKET_PORT = 8014;//Socket端口
-    public static String SOCKET_IP = "www.61120.cn";
-    public static String WEB_IP = "https://www.61120.cn";
-
-//    public static int SOCKET_PORT = BuildConfig.SOCKET_PORT;//Socket端口
-//    public static String SOCKET_IP = BuildConfig.SOCKET_IP; //Socket IP
-//    public static String WEB_IP = BuildConfig.WEB_IP;//服务器地址
+    public static int SOCKET_PORT = BuildConfig.SOCKET_PORT;//Socket端口
+    public static String SOCKET_IP = BuildConfig.SOCKET_IP; //Socket IP
+    public static String WEB_IP = BuildConfig.WEB_IP;//服务器地址
 
     //分享地址
     public static String SHARE_WEB = BuildConfig.SHARE_WEB;//六一健康分享地址
