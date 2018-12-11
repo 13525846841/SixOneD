5f86b6b77cdd9bea472a2e3892ade2bf7eb3824b
Laddie <762900942@qq.com>
2018-12-11 10:30:40 +0800

    修改configs文件

--- app/src/main/java/com/yksj/consultation/constant/Configs.java
+++ app/src/main/java/com/yksj/consultation/constant/Configs.java
@@ -7,24 +7,6 @@ import com.yksj.consultation.sonDoc.BuildConfig;
  * Used for socket,http地址端口链接
  */
 public class Configs {
-    //六一正式
-//    public static int SOCKET_PORT = 8014;//Socket端口
-//    public static String SOCKET_IP = "www.61120.cn";
-//    public static String WEB_IP = "https://www.61120.cn";
-
-//    public static int SOCKET_PORT = 8014;//Socket端口
-//    public static String SOCKET_IP = "www.61120.vip";
-//    public static String WEB_IP = "https://www.61120.vip";
-
-//    六一测试
-//    public static int SOCKET_PORT = 8015;//Socket端口
-//    public static String SOCKET_IP = "61.jiansion.com";
-//    public static String WEB_IP = "https://61.jiansion.com";
-
-    //本地测试
-//    public static int SOCKET_PORT = 8014;//Socket端口
-//    public static String SOCKET_IP = "www.61120.cn";
-//    public static String WEB_IP = "https://www.61120.cn";
 
     public static int SOCKET_PORT = BuildConfig.SOCKET_PORT;//Socket端口
     public static String SOCKET_IP = BuildConfig.SOCKET_IP; //Socket IP
