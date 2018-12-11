f6fa712cc68032296b0dd04c5da61e13450782f4
Laddie <762900942@qq.com>
2018-12-11 11:43:52 +0800

    Config文件 修改服务器地址

--- app/src/main/java/com/yksj/consultation/constant/Configs.java
+++ app/src/main/java/com/yksj/consultation/constant/Configs.java
@@ -8,13 +8,13 @@ import com.yksj.consultation.sonDoc.BuildConfig;
  */
 public class Configs {
     //本地测试
-    public static int SOCKET_PORT = 8014;//Socket端口
-    public static String SOCKET_IP = "www.61120.cn";
-    public static String WEB_IP = "https://www.61120.cn";
+//    public static int SOCKET_PORT = 8014;//Socket端口
+//    public static String SOCKET_IP = "www.61120.cn";
+//    public static String WEB_IP = "https://www.61120.cn";
 
-//    public static int SOCKET_PORT = BuildConfig.SOCKET_PORT;//Socket端口
-//    public static String SOCKET_IP = BuildConfig.SOCKET_IP; //Socket IP
-//    public static String WEB_IP = BuildConfig.WEB_IP;//服务器地址
+    public static int SOCKET_PORT = BuildConfig.SOCKET_PORT;//Socket端口
+    public static String SOCKET_IP = BuildConfig.SOCKET_IP; //Socket IP
+    public static String WEB_IP = BuildConfig.WEB_IP;//服务器地址
 
     //分享地址
     public static String SHARE_WEB = BuildConfig.SHARE_WEB;//六一健康分享地址
